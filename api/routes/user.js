8c718005ca91c9d89c9acf693e7ff6dca079d681
Lennard <lennard.gerdes@gmx.net>
2018-08-16 16:59:12 +0200

    Create API call for users

--- api/routes/user.js
+++ api/routes/user.js
@@ -9,7 +9,8 @@ router.get('/', (req, res, next) =>{
 
 router.post('/', (req, res, next) =>{
     res.status(200).json({
-        message: 'Handling Post'
+        message: 'Handling Po' +
+            'st'
     })
 });
 
