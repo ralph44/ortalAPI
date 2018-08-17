b9720bd79d25d9c34b5ed3cff39f857e49d37898
Lennard <lennard.gerdes@gmx.net>
2018-08-17 14:54:30 +0200

    Error Handling

--- app.js
+++ app.js
@@ -5,4 +5,17 @@ const userRoutes = require('./api/routes/user');
 
 app.use('/user', userRoutes);
 
+app.use((req, res, next) =>{
+    const error = new Error("Not found");
+    error.status(404);
+});
+
+app.use((error, req, res, next) =>{
+   res.status(error.status || 500);
+   res.json({
+       error: {
+           message: error.message
+       }
+   })
+});
 module.exports = app;
