6486acb4ef5e83b9f6f0bd53cb6120a9586fc365
Lennard <lennard.gerdes@gmx.net>
2018-08-17 15:25:04 +0200

    Handling CORS

--- api/routes/user.js
+++ api/routes/user.js
@@ -21,8 +21,6 @@ router.post('/', (req, res, next) =>{
 router.get('/:user/:pw', (req, res, next) =>{
     const user1 = req.params.user;
     const pw = req.params.pw;
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     if (user1 === '1' && pw == "1111"){
         res.status(200).json({
             message: 'hella'
--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const userRoutes = require('./api/routes/user');
 
 app.use(bodyParser.urlencoded({extend: false}));
 app.use(bodyParser.json());
+app.use((req, res,next) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Method', 'PUT', 'POST', 'PATCH', 'DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use('/user', userRoutes);
 
